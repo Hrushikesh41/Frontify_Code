f040ea75bdac93c85df3285654675ae688aedeb1
Hrushikesh41 <hrushikesh.kok@gmail.com>
2024-09-17 06:57:45 +0530

    Improvised Package for new Version

--- bin.js
+++ bin.js
@@ -10,7 +10,7 @@ const prompt = require('prompt-sync')();
 
 console.log(
     cli_color.cyanBright.bold(
-        `\nHeyy, I am Frontify 💥💥. I'm here to deliver you your Bioler Plate: \n\n1. Boiler PLate with all required packages using create-react-app\n2 Boiler PLate with all required packages using vite .\n`
+        `\nHeyy, I am Frontify 💥💥. I'm here to deliver you your Bioler Plate.`
     )
 );
 
@@ -31,6 +31,9 @@ inquirer.prompt([
         fs.unlinkSync(`${process.cwd()}/${name}/src/reportWebVitals.js`);
         fs.unlinkSync(`${process.cwd()}/${name}/src/setupTests.js`);
         fs.unlinkSync(`${process.cwd()}/${name}/src/App.test.js`);
+        fs.unlinkSync(`${process.cwd()}/${name}/public/favicon.ico`);
+        fs.unlinkSync(`${process.cwd()}/${name}/public/logo192.png`);
+        fs.unlinkSync(`${process.cwd()}/${name}/public/logo512.png`);
         fs.mkdirSync(`${process.cwd()}/${name}/src/components`);
         fs.mkdirSync(`${process.cwd()}/${name}/src/assets`);
         fs.mkdirSync(`${process.cwd()}/${name}/src/pages`);
